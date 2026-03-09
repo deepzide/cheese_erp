23f99e31a15f049e0351749ee82fdb2051dd86c0
itsyosefali <joeyxjoey123@gmail.com>
2026-03-09 16:40:13 +0200

    feat: create Cheese Contact doctype with associated test and client scripts, and update its definition with field property adjustments.

---
+++ cheese/cheese/doctype/cheese_contact/cheese_contact.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, itsyosefali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Cheese Contact", {
+// 	refresh(frm) {
+
+// 	},
+// });
